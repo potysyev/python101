833f073faa0df99b0299f09dfc470f785a9c74ad
Victor Potysyev <potysyev@gmail.com>
2022-04-06 22:04:28 +1100

    HW2 commit

--- lesson2/main1.py
+++
@@ -1,3 +0,0 @@
-usr_list = [1, "d", True, (2, 3), [4, 5], {6, 7}, {"key": "value", "key2": "value3"}]
-for item in usr_list:
-    print(type(item))
--- lesson2/main4.py
+++
@@ -1,4 +0,0 @@
-usr_string = input("input several words separated with spaces ")
-string_list = usr_string.split()
-for i, el in enumerate(string_list):
-    print(i + 1, el[:10])
