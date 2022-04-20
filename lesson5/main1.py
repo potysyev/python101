c55b0047ed79c0232d97916121ba753d306af20b
Victor Potysyev <potysyev@gmail.com>
2022-04-20 11:16:18 +1100

    HW6 commit

--- lesson5/main1.py
+++ lesson5/main1.py
@@ -1,5 +1,3 @@
 with open("out_file.txt", "w", encoding='utf-8') as f_obj:
-    usr_str = " "
-    while usr_str != "":
-        usr_str = input("Input data to write to file or empty line to finish: ")
+    while (usr_str := input("Input data to write to file or empty line to finish: ")) != "":
         f_obj.write(usr_str + "\n")
